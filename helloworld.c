5a4a7cf53deb80693146953af502ecee9d1bcb44
Dan Polityka <73758508+benochi@users.noreply.github.com>
2024-10-30 08:41:26 -0500

    basics

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
-  char one;
-  one = -128;
+  unsigned char one; //-128 - 127 if signed 0-255 unsigned.
+  one = 255;
   printf("One value is: %d", one);
   return 0;
 }
